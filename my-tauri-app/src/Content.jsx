09e2f67a0443633866672fb1cceee73445991b58
Nhan <nhanphamthe2410@gmail.com>
2025-07-17 15:26:16 +0700

    tao project

---
+++ my-tauri-app/src/Content.jsx
@@ -0,0 +1,9 @@
+function Content(){
+    return(
+        <>
+            <p>Day la phan content</p>
+        </>
+
+    );
+}
+export default Content
---
+++ my-tauri-app/src/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return (
+        <header>
+            <h1>My website</h1>
+            <p>1 website voi react</p>
+            <hr></hr>
+        </header>
+
+    );
+}
+export default Header
